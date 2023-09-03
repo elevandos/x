a250a74efde79e4e8930f8a7be500c2acc2e2501
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-09-03 12:11:33 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -86,7 +86,7 @@ export default function About() {
                 gelen atölyelerin, dans, müzik ve sürpriz birçok farklı
                 deneyimin yer alacağı festivalde dinamizmi maksimum seviyede
                 yaşamak için 23-24 Eylül&apos;ü ajandanıza kaydedin! 
-                
+              </br>
                 Festivale ücretsiz katılım için detaylar çok yakında burada!
                 
               </p>
