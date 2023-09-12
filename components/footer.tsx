deb77732f4a2218f7fad6fc136c224c2aa4d25dd
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-09-12 14:03:31 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,18 @@
+import React, { useState } from "react";
 import Image from "next/image";
 import BoynerLogo from "@/public/boyner-logo-dark-bg.svg";
 import AppStoreLogo from "@/public/assets/kit/app-store-logo.png";
 import GooglePlayLogo from "@/public/assets/kit/google-play-logo.png";
 import AppGalleryLogo from "@/public/assets/kit/app-gallery-logo.png";
-import { useState } from 'react'; // useState ekleyin
+
+// İlgili bileşenleri içe aktarın
 import {
   Dialog,
   DialogTrigger,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 
 const navigation = {
@@ -40,34 +42,21 @@ const apps = [
     imageAlt: "App Gallery",
   },
 ];
+
+export default function Footer() {
   return (
     <footer className="relative flex flex-col justify-between space-y-10 bg-black">
       <div className="mx-auto flex max-w-7xl flex-col items-center space-y-10 overflow-hidden pt-32 sm:px-6 lg:px-8">
         <div className="mb-16 flex items-center justify-center p-2">
           <Image src={BoynerLogo} alt="" className="h-auto w-64 lg:w-96" />
         </div>
-        <nav
-          className="flex justify-center space-x-3 text-xs tracking-tighter"
-          aria-label="Footer"
-        >
-          {navigation.main.map((item) => (
-            <div key={item.name} className="pb-6">
-              <a
-                href={item.href}
-                className="text-sm leading-6 text-white hover:text-muted-foreground"
-              >
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </nav>
-        {/* Uygulama bağlantıları */}
+        {/* Uygulama indirme bağlantıları */}
         <div className="flex flex-wrap justify-center space-x-6 p-2">
           {apps.map((app) => (
             <a
               key={app.name}
               href={app.href}
-              className="mb-2 text-sm leading-6 text-white hover:text-gray-900 cursor-pointer"
+              className="mb-2 text-sm leading-6 text-white hover:text-gray-900"
             >
               <Image
                 src={app.imageSrc}
@@ -77,22 +66,41 @@ const apps = [
             </a>
           ))}
         </div>
-        {/* Popup içeriği */}
-        {isPopupOpen && (
-          <Dialog>
-            <DialogTrigger>
-              <div className="fixed inset-0 z-50 bg-black bg-opacity-70"></div>
-            </DialogTrigger>
-            <DialogContent className="z-50">
-              <DialogHeader>
-                <DialogTitle>{popupContent}</DialogTitle>
-              </DialogHeader>
-              <DialogDescription>
-                Kullanım Koşulları Örnek Metni
-              </DialogDescription>
-            </DialogContent>
-          </Dialog>
-        )}
+        {/* Ana navigasyon menüsü */}
+        <nav
+          className="flex justify-center space-x-3 text-xs tracking-tighter"
+          aria-label="Footer"
+        >
+          {navigation.main.map((item) => (
+            <div key={item.name} className="pb-6">
+              {item.name === "Kullanım Koşulları" ? (
+                <Dialog>
+                  <DialogTrigger>
+                    <button className="text-sm leading-6 text-white hover:text-muted-foreground">
+                      {item.name}
+                    </button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Kullanım Koşulları</DialogTitle>
+                    </DialogHeader>
+                    <DialogDescription>
+                      {/* Gerçek kullanım koşulları metnini buraya ekleyin */}
+                      Örnek Kullanım Koşulları metni.
+                    </DialogDescription>
+                  </DialogContent>
+                </Dialog>
+              ) : (
+                <a
+                  href={item.href}
+                  className="text-sm leading-6 text-white hover:text-muted-foreground"
+                >
+                  {item.name}
+                </a>
+              )}
+            </div>
+          ))}
+        </nav>
       </div>
     </footer>
   );
