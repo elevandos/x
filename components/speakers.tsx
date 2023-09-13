e65f6dcbf5ee184a9c0a8fd5d2495fd757c5cf58
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-09-13 19:35:41 +0300

    Update speakers.tsx

--- components/speakers.tsx
+++ components/speakers.tsx
@@ -195,7 +195,7 @@ const Speakers = () => {
                         {person.role}
                       </p>
                     </DialogTrigger>
-                    <DialogContent className="hide-scrollbars overflow-scroll">
+                    <DialogContent className="max-h-[400px] overflow-auto">
                       <DialogHeader>
                         <DialogTitle>{person.name}</DialogTitle>
                       </DialogHeader>
