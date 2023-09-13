bc4483ef6eae45e47fcc4e187fb159f4f6b2340a
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-09-13 14:30:56 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -72,9 +72,9 @@ export default function Footer() {
           aria-label="Footer"
         >
           {navigation.main.map((item) => (
-            <div key={item.name} style={overflow:auto} className="pb-6">
+            <div key={item.name} className="pb-6">
               {item.name === "Kullanım Koşulları" ? (
-                <Dialog>
+                <Dialog  style={{scrollBehavior:'smooth'}}>
                   <DialogTrigger>
                     <button className="text-sm leading-6 text-white hover:text-muted-foreground">
                       {item.name}
