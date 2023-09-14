fe91f0a7e7a558b38bc496e3c57d8b21b65e0b1f
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-09-14 20:51:07 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -41,7 +41,7 @@ function ResponsiveImages() {
 
       {/* Images for mobile screens */}
       <div className="flex flex-wrap justify-center sm:hidden">
-        <a href="BURAYA_PLAYSTORE_LINKINIZI_EKLEYIN">
+        <a href="https://play.google.com/store/apps/details?id=tr.com.boyner.dynamic">
           <Image src={GooglePlay} alt="Google Play Logo" className="mt-10" />
         </a>
       </div>
